921748709ec8c5c80f411f954a8b1c918e09582d
Seedgou <i@zczc.cz>
2022-04-13 08:12:54 +0000

    add main.go

--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"path"
+	"time"
+
+	"github.com/rwv/mongodb-backup-s3/mongodb"
+	"github.com/rwv/mongodb-backup-s3/storage"
+)
+
+func backup(mongodbUri string) error {
+	log.Print("Starting backup...")
+	log.Print("Mongodb Uri: " + mongodbUri)
+
+	now := time.Now()
+	uploadFilename := "mongodb_dump_" + now.Format("20060102150405") + ".gz"
+	log.Print("Backup Filename: " + uploadFilename)
+
+	tempDir, err := os.MkdirTemp("", "mongodb-backup-s3-")
+
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(tempDir)
+
+	log.Print("Temp Dir: " + tempDir)
+
+	tempFilename := path.Join(tempDir, "dump.gz")
+	log.Print("Temp File: " + tempFilename)
+
+	log.Print("Dumping to temp file...")
+	err = mongodb.Dump(mongodbUri, tempFilename)
+	if err != nil {
+		return err
+	}
+
+	storage := *storage.GetInstance()
+
+	log.Print("Uploading to S3...")
+	err = storage.Upload(tempFilename, uploadFilename)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello, world.")
+	mongodbUri := os.Getenv("MONGODB_URI")
+
+	err := backup(mongodbUri)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
