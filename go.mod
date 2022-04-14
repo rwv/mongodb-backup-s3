1f61d1eff4fe0c6da32514f30e4d474363f34da9
Seedgou <i@zczc.cz>
2022-04-14 01:19:42 +0000

    use github.com/mongodb/mongo-tools/mongodump

--- go.mod
+++ go.mod
@@ -6,6 +6,21 @@ require github.com/aws/aws-sdk-go v1.43.38
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/jessevdk/go-flags v1.4.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/klauspost/compress v1.10.1 // indirect
+	github.com/mongodb/mongo-tools v0.0.0-20220127135714-e2842eb54930 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.0.2 // indirect
+	github.com/xdg-go/stringprep v1.0.2 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.mongodb.org/mongo-driver v1.7.1 // indirect
+	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- mongodb/Dump.go
+++ mongodb/Dump.go
@@ -1,11 +1,23 @@
 package mongodb
 
 import (
-	"log"
 	"os"
-	"os/exec"
+	"time"
+
+	"github.com/mongodb/mongo-tools/common/progress"
+	"github.com/mongodb/mongo-tools/mongodump"
+
+	"log"
 	"path"
 )
+
+const (
+	progressBarLength   = 24
+	progressBarWaitTime = time.Second * 3
+)
+
+const VersionStr = "100.5.2"
+const GitCommit = "e2842eb54930"
 
 func Dump(mongodbUri string, target string) error {
 	tempDir, err := os.MkdirTemp("", "mongodb-backup-s3-")
@@ -17,12 +29,29 @@ func Dump(mongodbUri string, target string) error {
 
 	tempFile := path.Join(tempDir, "dump")
 
-	// run mongodump
-	cmd := exec.Command("/usr/bin/mongodump", "--uri="+mongodbUri, "--gzip", "--archive="+tempFile)
-	log.Print(cmd)
-	err = cmd.Run()
+	args := []string{"--uri=" + mongodbUri, "--gzip", "--archive=" + tempFile}
 
+	opts, err := mongodump.ParseOptions(args, VersionStr, GitCommit)
 	if err != nil {
+		return err
+	}
+
+	progressManager := progress.NewBarWriter(log.Writer(), progressBarWaitTime, progressBarLength, false)
+	progressManager.Start()
+	defer progressManager.Stop()
+
+	dump := mongodump.MongoDump{
+		ToolOptions:     opts.ToolOptions,
+		OutputOptions:   opts.OutputOptions,
+		InputOptions:    opts.InputOptions,
+		ProgressManager: progressManager,
+	}
+
+	if err = dump.Init(); err != nil {
+		return err
+	}
+
+	if err = dump.Dump(); err != nil {
 		return err
 	}
 
@@ -31,5 +60,6 @@ func Dump(mongodbUri string, target string) error {
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
